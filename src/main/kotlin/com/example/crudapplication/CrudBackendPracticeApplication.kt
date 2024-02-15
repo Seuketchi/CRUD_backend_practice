a69827c4eb7658b1bb872407e21c704c1b24365a
tristan <tristan.jadman@g.msuiit.edu.ph>
2024-02-16 06:26:11 +0800

    initial commit

---
+++ src/main/kotlin/com/example/crudapplication/CrudBackendPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.crudapplication
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CrudBackendPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<CrudBackendPracticeApplication>(*args)
+}
