668683edb79613e7562f7e23ab8bab78d4f9d92c
tristan <tristan.jadman@g.msuiit.edu.ph>
2024-02-16 16:52:09 +0800

    Added CRUD to NotesController

--- src/main/kotlin/com/example/crudapplication/controllers/NotesController.kt
+++ src/main/kotlin/com/example/crudapplication/controllers/NotesController.kt
@@ -1,9 +1,16 @@
 package com.example.crudapplication.controllers
 
 import com.example.crudapplication.model.Notes
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 import java.util.ArrayList
@@ -13,10 +20,36 @@ import java.util.ArrayList
 class NotesController {
 
     @GetMapping("notes")
-    fun getNotes(): ResponseEntity<List<Notes>> {
-        val listOfNotes = ArrayList<Notes>()
-        listOfNotes.add(Notes(1,"Title", "content"))
-        listOfNotes.add(Notes(2,"Title2", "content2"))
-        return ResponseEntity.ok(listOfNotes);
+    fun getListOfNotes(): ResponseEntity<List<Notes>> {
+        val notes = ArrayList<Notes>()
+        notes.add(Notes(1, "Title", "content"))
+        notes.add(Notes(2, "Title2", "content2"))
+        return ResponseEntity.ok(notes);
+    }
+
+    @GetMapping("notes/{id}")
+    fun getNote(@PathVariable id: Long): Notes {
+        return Notes(id, "Title", "content")
+    }
+
+    @PostMapping("notes/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun createNote(@RequestBody note: Notes): ResponseEntity<Notes> {
+        println(note.title)
+        println(note.content)
+        return ResponseEntity(note, HttpStatus.CREATED)
+    }
+
+    @PutMapping("notes/{id}/update")
+    fun updateNote(@RequestBody note: Notes, @PathVariable("id") notesId: Long): ResponseEntity<Notes> {
+        println(note.title)
+        println(note.content)
+        return ResponseEntity.ok(note)
+    }
+
+    @DeleteMapping("notes/{id}/delete")
+    fun deleteNote(@PathVariable("id") notesId: Long):ResponseEntity<String> {
+        println(notesId)
+        return ResponseEntity.ok("Pokemon deleted successfully")
     }
 }
