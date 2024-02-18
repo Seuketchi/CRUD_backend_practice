de48379bc2df309cbde3e4941722c1e15df9896e
tristan <tristan.jadman@g.msuiit.edu.ph>
2024-02-18 10:44:18 +0800

    Annotated @Repository to NotesRepository

--- src/main/kotlin/com/example/crudapplication/repository/NotesRepository.kt
+++ src/main/kotlin/com/example/crudapplication/repository/NotesRepository.kt
@@ -1,7 +1,10 @@
 package com.example.crudapplication.repository
 
+import com.example.crudapplication.dto.NotesDTO
 import com.example.crudapplication.model.Notes
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface NotesRepository : JpaRepository<Notes, Long>{
 }
